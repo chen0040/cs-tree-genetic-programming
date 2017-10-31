cea8935f6a9442b01f05dabf441eb05d9857e31e
xschen <xs0040@gmail.com>
2017-10-31 10:19:23 +0800

    spiral classification

---
+++ cs-tree-genetic-programming-samples-spiral-classification/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TreeGP.SpiralClassification
+{
+    using System.IO;
+    using System.Data;
+    using TreeGP.ComponentModels;
+    using TreeGP.ComponentModels.Operators;
+    using TreeGP.ComponentModels.Operators.Unary;
+    using TreeGP.ComponentModels.Operators.Binary;
+    using TreeGP.Distribution;
+
+    class Program
+    {
+        static DataTable LoadData(string filename)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("X");
+            table.Columns.Add("Y");
+            table.Columns.Add("Label");
+
+            int line_count = 0;
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line = reader.ReadLine();
+                int.TryParse(line, out line_count);
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] elements = line.Split(new char[] { '\t' });
+
+                    double x, y;
+                    int label;
+                    double.TryParse(elements[0].Trim(), out x);
+                    double.TryParse(elements[1].Trim(), out y);
+                    int.TryParse(elements[2].Trim(), out label);
+
+                    table.Rows.Add(x, y, label);
+                }
+            }
+            return table;
+        }
+        static void Main(string[] args)
+        {
+            DataTable table = LoadData("dataset.txt");
+
+            TGPConfig config = new TGPConfig("TGPConfig.xml");
+            //config.CrossoverRate = 0.35;
+            //config.MicroMutationRate = 0.15;
+            //config.MacroMutationRate = 0.45;
+            //config.ReproductionRate = 0.05;
+            config.ElitismRatio = 0.1;
+            TGPPop<TGPSolution> pop = new TGPPop<TGPSolution>(config);
+            pop.PopulationReplacement = TGPPop<TGPSolution>.PopulationReplacementMode.TinyGP;
+
+            pop.OperatorSet.AddOperator(new TGPOperator_Plus());
+            pop.OperatorSet.AddOperator(new TGPOperator_Minus());
+            pop.OperatorSet.AddOperator(new TGPOperator_Division());
+            pop.OperatorSet.AddOperator(new TGPOperator_Multiplication());
+            pop.OperatorSet.AddOperator(new TGPOperator_Sin());
+            pop.OperatorSet.AddOperator(new TGPOperator_Cos());
+            pop.OperatorSet.AddIfgtOperator();
+
+            for (int i = 1; i < 10; ++i)
+            {
+                pop.ConstantSet.AddConstant(string.Format("C{0}", i), i);
+            }
+
+            pop.VariableSet.AddVariable("x");
+            pop.VariableSet.AddVariable("y");
+
+            pop.CreateFitnessCase += (index) =>
+                {
+                    SpiralFitnessCase fitness_case = new SpiralFitnessCase();
+                    fitness_case.X = double.Parse(table.Rows[index]["X"].ToString());
+                    fitness_case.Y = double.Parse(table.Rows[index]["Y"].ToString());
+                    fitness_case.Label = int.Parse(table.Rows[index]["Label"].ToString());
+
+                    return fitness_case;
+                };
+
+            pop.GetFitnessCaseCount += () =>
+                {
+                    return table.Rows.Count;
+                };
+
+            pop.EvaluateObjectiveForSolution += (fitness_cases, solution, objective_index) =>
+            {
+                double fitness = 0;
+                for (int i = 0; i < fitness_cases.Count; i++)
+                {
+                    SpiralFitnessCase fitness_case = (SpiralFitnessCase)fitness_cases[i];
+                    int correct_y = fitness_case.Label;
+                    int computed_y = fitness_case.ComputedLabel;
+                    fitness += (correct_y == computed_y) ? 0 : 1;
+                }
+
+                return fitness;
+            };
+
+
+            pop.BreedInitialPopulation();
+
+
+            while (!pop.IsTerminated)
+            {
+                pop.Evolve();
+                Console.WriteLine("Spiral Classification Generation: {0}", pop.CurrentGeneration);
+                Console.WriteLine("Global Fitness: {0}\tCurrent Fitness: {1}", pop.GlobalBestProgram.Fitness, pop.FindFittestProgramInCurrentGeneration().Fitness);
+                Console.WriteLine("Global Best Solution:\n{0}", pop.GlobalBestProgram);
+            }
+
+            Console.WriteLine(pop.GlobalBestProgram.ToString());
+        }
+    }
+}
---
+++ cs-tree-genetic-programming-samples-spiral-classification/SpiralFitnessCase.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TreeGP.SpiralClassification
+{
+    using TreeGP.Core.ProblemModels;
+    using TreeGP.ComponentModels;
+
+    public class SpiralFitnessCase : IGPFitnessCase
+    {
+        private double mX;
+        private double mY;
+        private int mLabel;
+        private int mComputedLabel;
+
+        public int ComputedLabel
+        {
+            get { return mComputedLabel; }
+        }
+
+        public int Label
+        {
+            get { return mLabel; }
+            set { mLabel = value; }
+        }
+
+        public double X
+        {
+            get { return mX; }
+            set { mX = value; }
+        }
+
+        public double Y
+        {
+            get { return mY; }
+            set { mY = value; }
+        }
+
+        public void StoreOutput(object result, int tree_index)
+        {
+            double dresult = (double)result;
+            if (dresult < 0.5)
+            {
+                mComputedLabel = -1;
+            }
+            else
+            {
+                mComputedLabel = 1;
+            }
+        }
+
+        public bool QueryInput(string variable_name, out object input)
+        {
+            input = 0;
+            if (variable_name == "x")
+            {
+                input = mX;
+                return true;
+            }
+            else if (variable_name == "y")
+            {
+                input = mY;
+                return true;
+            }
+            return false;
+        }
+
+
+        public int GetInputCount()
+        {
+            return 2;
+        }
+
+
+    }
+}
