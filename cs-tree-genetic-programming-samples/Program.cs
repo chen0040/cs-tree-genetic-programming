cf3e9e1c3c505b92acc4e9a43e86d581d6deeea7
xschen <xs0040@gmail.com>
2017-10-31 10:07:34 +0800

    xml config

--- cs-tree-genetic-programming-samples/Program.cs
+++ cs-tree-genetic-programming-samples/Program.cs
@@ -2,14 +2,127 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace cs_tree_genetic_programming_samples
+namespace TreeGP.SymbolicRegression
 {
+    using System.IO;
+    using System.Data;
+    using TreeGP.ComponentModels;
+    using TreeGP.ComponentModels.Operators.Unary;
+    using TreeGP.ComponentModels.Operators.Binary;
+    using TreeGP.Distribution;
+    using TreeGP.AlgorithmModels.Selection;
+
+    /// <summary>
+    /// This example is based on Chapter 4 of "A Field Guide to Genetic Programming"
+    /// </summary>
     class Program
     {
+        static double FunctionXY(double x)
+        {
+            return x * x + x + 1;
+        }
+
+        static DataTable LoadData()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("X");
+            table.Columns.Add("Y");
+
+            double lower_bound = -1.0;
+            double upper_bound = 1.0;
+
+            double interval = 0.1;
+
+            for (double x = lower_bound; x <= upper_bound; x+=interval)
+            {
+                table.Rows.Add(x, FunctionXY(x));
+            }
+
+
+            return table;
+        }
         static void Main(string[] args)
         {
+            DataTable table = LoadData();
+
+            TGPConfig config = new TGPConfig("TGPConfig.xml");
+
+            // The problem is to minimize the sum of errors between predicted and actual function
+            config.IsMaximization = false;
+
+            // As specified in Chapter 4 of "A Field Guide to Genetic Programming"
+            config.PopulationSize = 4;
+            config.ElitismRatio = 0; //non elitist
+            config.CrossoverRate = 0.5; // subtree crossover rate set to 0.5
+            config.MacroMutationRate = 0.25; // subtree mutation rate set to 0.25; 
+            config.MicroMutationRate = 0.0; // point mutation rate set to 0.0
+            config.ReproductionRate = 0.25; // reproduction rate set to 0.25
+            config.NormalizeEvolutionRates();
+            //Question 1: Is the performance normal for the GP?
+
+            config.MaximumDepthForCreation = 2;
+            config.MaximumDepthForCrossover = 2; // no tree size limit by setting a very large max depth
+            config.MaximumDepthForMutation = 2; 
+
+            TGPPop<TGPSolution> pop = new TGPPop<TGPSolution>(config);
+            pop.ReproductionSelectionInstruction = new SelectionInstruction_RouletteWheel<TGPSolution>(); //use roulette wheel selection 
+
+            // Function Set = {+, -, %, *} where % is protected division that returns 1 if the denominator is 0
+            pop.OperatorSet.AddOperator(new TGPOperator_Plus());
+            pop.OperatorSet.AddOperator(new TGPOperator_Minus());
+            pop.OperatorSet.AddOperator(new TGPOperator_Division());
+            pop.OperatorSet.AddOperator(new TGPOperator_Multiplication());
+
+            // Terminal Set = {R, x}
+            pop.ConstantSet.AddConstant("R", DistributionModel.GetUniform()* 10.0 - 5.0);
+            pop.VariableSet.AddVariable("x");
+
+            pop.CreateFitnessCase += (index) =>
+            {
+                SymRegFitnessCase fitness_case = new SymRegFitnessCase();
+                fitness_case.X = double.Parse(table.Rows[index]["X"].ToString());
+                fitness_case.Y = double.Parse(table.Rows[index]["Y"].ToString());
+
+                return fitness_case;
+            };
+
+            pop.GetFitnessCaseCount += () =>
+            {
+                return table.Rows.Count;
+            };
+
+            pop.EvaluateObjectiveForSolution += (fitness_cases, solution, objective_index) =>
+            {
+                double sum_of_error = 0;
+                for (int i = 0; i < fitness_cases.Count; i++)
+                {
+                    SymRegFitnessCase fitness_case = (SymRegFitnessCase)fitness_cases[i];
+                    double correct_y = fitness_case.Y;
+                    double computed_y = fitness_case.ComputedY;
+                    sum_of_error += System.Math.Abs(correct_y - computed_y);
+                }
+
+                return sum_of_error;
+            };
+
+
+            pop.BreedInitialPopulation();
+
+            double error = pop.GlobalBestProgram.ObjectiveValue;
+            while (error > 0.1)
+            {
+                pop.Evolve();
+                error = pop.GlobalBestProgram.ObjectiveValue;
+
+                Console.WriteLine("Symbolic Regression Generation: {0}", pop.CurrentGeneration);
+                Console.WriteLine("Minimum Error: {0}", error.ToString("0.000"));
+                Console.WriteLine("Global Best Solution:\n{0}", pop.GlobalBestProgram);
+                
+            }
+
+            Console.WriteLine(pop.GlobalBestProgram.ToString());
+
         }
     }
 }
---
+++ cs-tree-genetic-programming-samples/SymRegFitnessCase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TreeGP.SymbolicRegression
+{
+    using TreeGP.Core.ProblemModels;
+    using TreeGP.ComponentModels;
+
+    public class SymRegFitnessCase : IGPFitnessCase
+    {
+        private double mX;
+        private double mY;
+        private double mComputedY;
+
+        public double ComputedY
+        {
+            get { return mComputedY; }
+        }
+
+        public double X
+        {
+            get { return mX; }
+            set { mX = value; }
+        }
+
+        public double Y
+        {
+            get { return mY; }
+            set { mY = value; }
+        }
+
+        public void StoreOutput(object result, int tree_index)
+        {
+            mComputedY = (double)result;
+        }
+
+        public bool QueryInput(string variable_name, out object input)
+        {
+            input = 0;
+            if (variable_name == "x")
+            {
+                input = mX;
+                return true;
+            }
+            
+            return false;
+        }
+
+
+        public int GetInputCount()
+        {
+            return 1;
+        }
+
+
+    }
+}
